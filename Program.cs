2ed456ca62536dc2d5f07c263660eb55d7bf885d
Elinaphinphat <enaphinphat@wsutech.edu>
2024-10-17 13:00:58 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Insert Length: ");
+        int UserInputLength = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please Insert Width: ");
+        int UserInputWidth = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The area of this rectangle is " + (UserInputLength * UserInputWidth));
+    }
+}
